4d1dffa76f12dd4028901c4e965c9a7efc43e43e
PENG,CHAO-WEI <alanwei1202@gmail.com>
2019-03-03 23:14:23 +0800

    [add] design output formation

--- Minimum Time Difference_539/Program.cs
+++ Minimum Time Difference_539/Program.cs
@@ -10,6 +10,31 @@ namespace Minimum_Time_Difference_539
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("----------- 最小時間差計算測試 ------------");
+            Console.WriteLine("--------- 格式 [ 00:00 - 23:59 ] ---------");
+            bool bFinished = false;
+            List<string> timePoints = new List<string>();
+            do
+            {
+                Console.Write("請輸入時間點 (結束請按 [Esc])：");
+                string currentPoint = ReadLineWithCancel();
+                if (currentPoint.Equals("t"))
+                {
+                    bFinished = true;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    timePoints.Add(currentPoint);
+                }
+            } while (!bFinished);
+
+            Console.WriteLine("\n----------- 計算結果 ------------");
+            foreach(string element in timePoints)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadLine();
         }
         public int FindMinDifference(IList<string> timePoints)
         {
@@ -70,6 +95,33 @@ namespace Minimum_Time_Difference_539
             // 23:59   00:00    >>>     
             // 24:00            >>>     24*60 = 1440
             // 1440 / 2 = 720   >>>     720 / 60 = 12
+        }
+        private static string ReadLineWithCancel()
+        {
+            string result = null;
+
+            StringBuilder buffer = new StringBuilder();
+
+            //The key is read passing true for the intercept argument to prevent
+            //any characters from displaying when the Escape key is pressed.
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter && info.Key != ConsoleKey.Escape)
+            {
+                Console.Write(info.KeyChar);
+                buffer.Append(info.KeyChar);
+                info = Console.ReadKey(true);
+            }
+
+            if (info.Key == ConsoleKey.Enter)
+            {
+                result = buffer.ToString();
+            }
+            else if(info.Key == ConsoleKey.Escape)
+            {
+                result = "t";
+            }
+
+            return result;
         }
     }
 }
